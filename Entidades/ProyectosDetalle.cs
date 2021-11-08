375e78b4046e1488591fc9b472dae2a7138b6a62
Alvaro Starling Hernandez Luzon <alvarostarling24@gmail.com>
2021-11-08 18:27:13 -0400

    Agregando la entidad ProyectosDetalle

---
+++ Entidades/ProyectosDetalle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_Alvaro_20190269.Entidades
+{
+    public class ProyectosDetalle
+    {
+        [Key]
+        public int ProyectoDetalleId { get; set; }
+        public int ProyectoId { get; set; }
+        public int TipoDeTareaId { get; set; }
+        public string Requerimiento { get; set; }
+        public int Tiempo { get; set; }
+
+        [ForeignKey("TipoDeTareaId")]
+        public virtual TiposDeTareas TiposDeTareas { get; set; }
+    }
+}
