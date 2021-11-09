d8e11764fa6c804e27a78f7c453500ec95f3ef4e
Alvaro Starling Hernandez Luzon <alvarostarling24@gmail.com>
2021-11-08 20:00:25 -0400

    Agregando Codido a los botones del registro Proyectos

--- UI/Registros/rProyectos.xaml.cs
+++ UI/Registros/rProyectos.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using P2_AP1_Alvaro_20190269.BLL;
+using P2_AP1_Alvaro_20190269.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,8 @@ namespace P2_AP1_Alvaro_20190269.UI.Registros
     /// </summary>
     public partial class rProyectos : Window
     {
+        private Proyectos proyecto = new Proyectos();
+
         public rProyectos()
         {
             InitializeComponent();
@@ -28,40 +31,118 @@ namespace P2_AP1_Alvaro_20190269.UI.Registros
             TipoDeTaeaComboBox.SelectedValuePath = "TipoDeTareaId";
             TipoDeTaeaComboBox.DisplayMemberPath = "Nombre";
         }
+        
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = proyecto;
+        }
+
+        private void Limpiar()
+        {
+            this.proyecto = new Proyectos();
+            this.DataContext = proyecto;
+        }
+
+        private bool Validar()
+        {
+            bool esValido = true;
+
+            if(DescripcionTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Falta la descripcion", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            if(DetalleDataGrid.Items.Count == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Falta la descripcion", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            return esValido;
+        }
 
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
+            var proyecto1 = ProyectosBLL.Buscar(proyecto.ProyectoId);
 
+            if (proyecto1 != null)
+            {
+                this.proyecto = proyecto1;
+                this.DataContext = proyecto;
+            }
+            else
+            {
+                Limpiar();
+                MessageBox.Show("No existe", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void AgregarFilaButton_Click(object sender, RoutedEventArgs e)
         {
+            var detalle = new ProyectosDetalle
+            {
+                ProyectoDetalleId = this.proyecto.ProyectoId,
+                TiposDeTareas = (TiposDeTareas)TipoDeTaeaComboBox.SelectedItem,
+                Requerimiento = RequerimientoTextBox.Text,
+                Tiempo = Utilidades.ToInt(TiempoTextBox.Text)
+            };
+
+            proyecto.TiempoTotal += Utilidades.ToInt(TiempoTextBox.Text);
+
+            proyecto.Detalle.Add(detalle);
+            Cargar();
 
         }
 
         private void RemoverFilaButton_Click(object sender, RoutedEventArgs e)
         {
+            var detalle = (ProyectosDetalle)DetalleDataGrid.SelectedItem;
 
+            if(DetalleDataGrid.Items.Count >= 1 && DetalleDataGrid.SelectedIndex <= DetalleDataGrid.Items.Count - 1)
+            {
+                proyecto.TiempoTotal -= detalle.Tiempo;
+
+                proyecto.Detalle.RemoveAt(DetalleDataGrid.SelectedIndex);
+                Cargar();
+            }
         }
 
         private void GuardarButton_Click(object sender, RoutedEventArgs e)
         {
+            if (!Validar())
+                return;
 
+            var paso = ProyectosBLL.Guardar(proyecto);
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Transferencia Exitosa", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Transferencia fallida", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void EliminarButton_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void RemoverFilaButton_Click_1(object sender, RoutedEventArgs e)
-        {
+            if (ProyectosBLL.Eliminar(proyecto.ProyectoId))
+            {
+                Limpiar();
+                MessageBox.Show("Registro Eliminado", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("No fue posible eliminar", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
 
         }
 
         private void NuevoButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Limpiar();
         }
     }
 }
